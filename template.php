4f9bcd3d3a599cd221595b2a285bb02b535aac50
Geoffrey Hing <geoff@terrorware.com>
2010-10-26 01:27:03 -0400

    Force home page's title to 'The Max Levine Ensemble'

--- template.php
+++ template.php
@@ -1,2 +1,9 @@
 <?php
 
+function tmle_preprocess_page(&$variables) {
+  if ($variables['is_front']) {
+    // Because we're using the Blogs module to implement the front page,
+    // the title defaults to 'Blogs'.  Change this.
+    $variables['title'] = $variables['head_title'] = 'The Max Levine Ensemble';
+  }
+}
